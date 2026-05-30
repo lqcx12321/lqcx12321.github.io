8c4f0d38206f8f9c232d40294685fda223366847
lqcx12321 <shj@bupt.edu.cn>
2026-05-30 17:57:56 +0800

    Site updated: 2026-05-30 17:57:56

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1780134475285";
+const VERSION = "1780135063848";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
